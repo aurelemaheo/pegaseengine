abc27d798f284d501846a8372e6d34a8a708478a
Aurele MAHEO <aurele.maheo@gmail.com>
2020-04-04 20:51:49 +0200

    source PATH with path to CMake github version

--- configure.sh
+++ configure.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -x
 
 # Add github version of CMake into PATH
-export PATH=/Users/aurele/git/CMake/bin:$PATH
+touch env_pegase_engine
+echo "PATH=$HOME/git/CMake/bin:$PATH" >> env_pegase_engine
+source env_pegase_engine
 
 cd thirdparty
 rm -rf *
 git clone https://github.com/open-source-parsers/jsoncpp.git
+cd jsoncpp
+cmake .
+make install
