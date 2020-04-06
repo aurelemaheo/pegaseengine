4a22f2dadf7d9aacbff3cc8528a2a439d43d2381
Aurele MAHEO <aurele.maheo@gmail.com>
2020-04-06 21:59:42 +0200

    Add class Reader and inherited class ReaderJson to the repository

---
+++ src/reader.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+/*
+* This class reads a configuration containing scene descriptions (objects, plan) 
+*/
+
+class Reader
+{};
---
+++ src/readerJson.hpp
@@ -0,0 +1,4 @@
+#include <iostream>
+
+class ReaderJson : public Reader
+{};
