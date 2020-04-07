face6a3b4750e0eb1c9834b00afb3f07963dbf13
Aurele MAHEO <aurele.maheo@gmail.com>
2020-04-08 00:24:10 +0200

    Class CollisionManager: add a vector structure containing pairs of solids involved into collision

--- src/collisionmanager.hpp
+++ src/collisionmanager.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <map>
+#include <vector>
 #include <thread>
 
 class CollisionManager
@@ -14,7 +15,8 @@ class CollisionManager
     void collListener();
 
   private:
-    std::map<int, float> mapSolidPos; // Map storing Solid identifiers as keys and for each solid its associated position
+    std::map<int, float> mapSolidPos; 		// Map storing Solid identifiers as keys and for each solid its associated position
+    std::vector<std::pair<int,int> > vectColls; // Vector where each item contains a pair of the 2 solids involved in the collision 
    //std::thread collThread(collListener);
 };
 
