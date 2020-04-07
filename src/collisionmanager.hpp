ac014389038f7369a56dfc7d1cf639cfb427d606
Aurele MAHEO <aurele.maheo@gmail.com>
2020-04-07 19:50:02 +0200

    CollisionManager class: Add a map containing solid identifiers and associated positions

--- src/collisionmanager.hpp
+++ src/collisionmanager.hpp
@@ -2,6 +2,7 @@
 #define COLLISION_MANAGER_H
 
 #include <iostream>
+#include <map>
 #include <thread>
 
 class CollisionManager
@@ -13,7 +14,7 @@ class CollisionManager
     void collListener();
 
   private:
-
+    std::map<int, float> mapSolidPos; // Map storing Solid identifiers as keys and for each solid its associated position
    //std::thread collThread(collListener);
 };
 
