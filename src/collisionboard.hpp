ecb0b71135764662c1e6d857c858d8a3254f97ac
Aurele MAHEO <aurele.maheo@gmail.com>
2020-04-18 22:21:16 +0200

    Add class CollisionBoard (observer)

---
+++ src/collisionboard.hpp
@@ -0,0 +1,14 @@
+#ifndef COLLISIONBOARD_H
+#define COLLISIONBOARD_H
+
+#include <iostream>
+
+#include "observerboard.hpp"
+
+/*
+* This class derives from the ObserverBoard abstract class and is specialized in observing collision information from the system
+*/
+class CollisionBoard
+{};
+
+#endif //COLLISIONBOARD_H
