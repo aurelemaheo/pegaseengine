435cb506df21b1be39ebf40060ed2b9fe31311ae
Aurele MAHEO <aurele.maheo@gmail.com>
2020-04-19 21:47:20 +0200

    Added const.hpp: define constants

---
+++ src/const.hpp
@@ -0,0 +1,10 @@
+#ifndef CONST_HPP
+#define CONST_HPP
+
+#define COLOR_BLUE "1;34
+#define COLOR_CYAN "1;36"
+#define COLOR_GREEN "1;32"
+#define COLOR_RED "1;31"
+#define COLOR_YELLOW "1;33"
+
+#endif //CONST_HPP
