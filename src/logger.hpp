d45ca0b6c286b882f82194a52a55ebeaf0b5aaa4
Aurele MAHEO <aurele.maheo@gmail.com>
2020-04-22 14:50:12 +0200

    class Log: define macro expansion + add args to Log constructor

--- src/logger.hpp
+++ src/logger.hpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 
+#define LOG(...) Log(__FILE__, __FUNCTION__, __LINE__)
+
 enum type_log
 {
   DEBUG,
@@ -11,16 +13,17 @@ enum type_log
   ERROR
 };
 
-class Logger
+class Log
 {
 
   public:
-    LOG(type_log type);	// Constructor
+    LOG(std::string fileName, std::string func, int line, type_log type);	// Constructor
     ~LOG();	// Destructor
     std::string getCurrentDate();
     std::string getCurrentTime();
     void write(std::string message);
     void destroy();
+    Logger operator <<(std::string message); 
   private:
    type_log msg_level;
    static ILogger& _instance; 
