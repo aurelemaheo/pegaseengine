7745c04094f94071983c1d41810c138004009e00
Aurele MAHEO <aurele.maheo@gmail.com>
2020-04-30 22:14:51 +0200

    class MemoryManager: check memory leaks in destructor

--- src/memorymanager.hpp
+++ src/memorymanager.hpp
@@ -1,6 +1,11 @@
 #ifndef MEMORYMANAGER_H
 #define MEMORYMANAGER_H
 
+#include <iostream>
+#include <string>
+
+#include "logger.hpp"
+
 struct TBlock
 {
   std::size_t 	Size;
@@ -13,8 +18,20 @@ typedef std::map<void *, TBlock> TBlockMap;
 class MemoryManager
 {
   public:
-   MemoryManager();
-   ~MemoryManager();
+   MemoryManager(){}
+   ~MemoryManager()
+   {
+     if(mBlocks.empty())
+     {
+       LOG(DEBUG) << "No memory leaks detected " << std::endl;
+     }
+     else 
+     {
+       LOG(ERROR) << "Memory leaks detected  " << std::endl;
+     }
+     
+   }
+
    void* operator new(std::size_t size, std::string file, int line)
    {}
    void* operator new[](std::size_t size, std::string file, int line)
@@ -28,6 +45,7 @@ class MemoryManager
    void Release(void* ptr, bool array); 
 
   private:
+    TBlockMap mBlocks;
 };
 
 #endif //MEMORYMANAGER_H
