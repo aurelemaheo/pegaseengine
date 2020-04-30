ec0594b0472395b97499f16ba690a8df380241fc
Aurele MAHEO <aurele.maheo@gmail.com>
2020-04-30 17:50:26 +0200

    Add a singleton for Memory Manager class

--- src/config.hpp
+++ src/config.hpp
@@ -5,9 +5,11 @@
 #include "pegaseengine.hpp"
 #include "renderer.hpp"
 #include "logger.hpp"
+#include "memorymanager.hpp"
 
 typedef Singleton<PegaseEngine>  pgSingleton;
 typedef Singleton<Renderer>  	 rendererSingleton;
 typedef Singleton<Logger>  	 loggerSingleton;
+typedef Singleton<MemoryManager> memmanagerSingleton;
 
 #endif //CONFIG_H
