8155aae4b162e1de4b55450bb515b0fff68809f9
Aurele MAHEO <aurele.maheo@gmail.com>
2020-05-01 18:54:10 +0200

    class MemoryManager: correct signature of Allocate method

--- src/memorymanager.hpp
+++ src/memorymanager.hpp
@@ -36,7 +36,7 @@ class MemoryManager
      }
    }
 
-   void* Allocate(std::size_t size, std::string file, std::string funcname, bool array);
+   void* Allocate(std::size_t size, std::string file, std::string funcname, int line, bool array);
    void Release(void* ptr, bool array); 
 
   private:
