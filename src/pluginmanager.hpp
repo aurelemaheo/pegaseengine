595529b1641db08068df0d2639a08ddbbd342c50
Aurele MAHEO <aurele.maheo@gmail.com>
2020-05-03 21:57:43 +0200

    Add class PluginManager

---
+++ src/pluginmanager.hpp
@@ -0,0 +1,16 @@
+#ifndef PLUGINMANAGER_H
+#define PLUGINMANAGER_H
+
+/**
+* This class loads .so libraries and use functions provided by the library
+*
+**/
+class PluginManager
+{
+  public:
+    PluginManager();
+    ~PluginManager();
+  private:
+};
+
+#endif //PLUGINMANAGER_H
