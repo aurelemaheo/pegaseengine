41d4b5295da21262ea3f4af4756be3ea3e5ef0b4
Aurele MAHEO <aurele.maheo@gmail.com>
2020-05-04 22:40:37 +0200

    Add class Quaternion

---
+++ src/maths/quaternion.hpp
@@ -0,0 +1,20 @@
+#ifndef QUATERNION_H
+#define QUATERNION_H
+
+template <typename T>
+class Quaternion
+{
+  public:
+    Quaternion();
+    Quaternion(T x, T y, T z, T w);
+    ~Quaternion();
+
+  private:
+    T _x;
+    T _y;
+    T _z;
+    T _w;
+ 
+};
+
+#endif //QUATERNION_H
