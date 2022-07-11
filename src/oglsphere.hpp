068d5c280b4835bdfd394cfd0fb54dc6d3422be4
Aurele MAHEO <aurele.maheo@gmail.com>
2022-07-11 17:35:31 +0200

    class OGLSphere: update

--- src/oglsphere.hpp
+++ src/oglsphere.hpp
@@ -3,6 +3,9 @@
 
 #include <vector>
 
+#define SECTOR_COUNT 36
+#define STACK_COUNT 18
+
 class OGLSphere
 {
     public:
@@ -12,11 +15,26 @@ class OGLSphere
         void setRadius(float radius);
 
     private:
-        float radius;    
+        float radius;   
+        int sectorCount;
+        int stackCount; 
         std::vector<unsigned int> indices;
+        std::vector<unsigned int> lineIndices;
         std::vector<float> vertices;
+        std::vector<float> interleavedVertices;
+        std::vector<float> normals;
+        std::vector<float> texCoords;
 
         void buildVertices();
+        void buildInterleavedVertices();
+        void addVertex(float x, float y, float z);
+        void addNormal(float x, float y, float z);
+        void addIndices(unsigned int i1, unsigned int i2, unsigned int i3);
+        std::vector<float> computeFaceNormal(float x1, float y1, float z1,
+                                         float x2, float y2, float z2,
+                                         float x3, float y3, float z3);
+        void addTexCoord(float s, float t);
+        void clearArrays();
 };
 
 #endif // OGLSPHERE_H
