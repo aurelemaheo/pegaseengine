4eb40879167f891f5167f56a405587a49c96e1b9
aurele <Aurele MAHEO>
2022-10-07 10:58:47 +0200

    bug fix: static cast Sphere to Body

--- src/sphere.hpp
+++ src/sphere.hpp
@@ -21,6 +21,13 @@ class Sphere: public Body
   private:
     int m_Id;
     double _radius;
-};
+
+    std::vector<float> vertices;
+    std::vector<float> normals;
+    std::vector<unsigned int> indices;
+
+    std::vector<float> interleavedVertices;
+    int interleavedStride;
+}; // class Sphere 
 
 #endif // SPHERE_H
